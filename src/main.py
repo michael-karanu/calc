5c5b36174f18042cdb068c49bb4734d2653c1668
root <root@localhost>
2022-05-19 11:59:48 +0300

    auto publish 1652950788297

---
+++ src/main.py
@@ -0,0 +1,10 @@
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+add = a+b
+mult= a*b
+division = a/b
+sub = a-b
+print("addition is",add)
+print("multiplication is",mult)
+print("division is",division)
+print("subtraction is",sub)
